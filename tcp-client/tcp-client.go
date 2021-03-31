871915f9f037838eff08015ca0c81c0542713b63
Practice Golang <practice.golang@gmail.com>
2021-03-31 16:14:14 +0900

    Add branch server2client

--- tcp-client/tcp-client.go
+++ tcp-client/tcp-client.go
@@ -4,22 +4,21 @@ import (
 	"bufio"
 	"fmt"
 	"net"
-	"os"
 )
 
 var delimiter = '\n'
 
 func main() {
-	con, _ := net.Dial("tcp", "127.0.0.1:8081")
+	con, _ := net.Dial("tcp", "127.0.0.1:7749")
 	fmt.Println("Client ready.")
 	fmt.Println("Ctrl+C to finish.")
 	for {
-		reader := bufio.NewReader(os.Stdin)
+		// reader := bufio.NewReader(os.Stdin)
 
-		fmt.Print("Msg for sending then press Enter: ")
+		// fmt.Print("Msg for sending then press Enter: ")
 
-		msgSend, _ := reader.ReadString(byte(delimiter))
-		fmt.Fprintf(con, msgSend+string(delimiter))
+		// msgSend, _ := reader.ReadString(byte(delimiter))
+		// fmt.Fprintf(con, msgSend+string(delimiter))
 
 		msgReceive, err := bufio.NewReader(con).ReadString(byte(delimiter))
 		if err != nil {
@@ -27,6 +26,7 @@ func main() {
 			panic(err)
 		}
 		if delimiter == '\n' {
+			fmt.Fprintf(con, msgReceive+string(delimiter)) // 차임벨클라 헬스 응답
 			fmt.Print("Response from server: " + msgReceive)
 		} else {
 			fmt.Println("Response from server: " + msgReceive)
--- tcp-server/tcp-server.go
+++ tcp-server/tcp-server.go
@@ -3,46 +3,67 @@ package main // import "tcp-server"
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"net"
 	"strings"
+	"time"
 	// only needed below for sample processing
 )
 
 var delimiter = '\n'
+var Msg chan string = make(chan string, 10)
+var Health chan string = make(chan string)
 
 func pcListener(fin chan int) {
-	fmt.Println("Server Listening TCP localhost:8081")
+	fmt.Println("Server Listening TCP this IP:7749")
 	fmt.Println("Ctrl+C to finish.")
 
-	ln, _ := net.Listen("tcp", ":8081")
+	ln, _ := net.Listen("tcp", ":7749")
 	defer ln.Close()
 
 	con, _ := ln.Accept()
 	defer con.Close()
 
 	for {
-		msg, _ := bufio.NewReader(con).ReadString(byte(delimiter))
+		select {
+		case request := <-Msg:
+			con.Write([]byte(request + string(delimiter)))
+		case health := <-Health:
+			healthMsg := strings.ToUpper(strings.TrimSuffix(health, string(delimiter)))
+			con.Write([]byte(healthMsg + string(delimiter)))
+			msg, _ := bufio.NewReader(con).ReadString(byte(delimiter))
+			if len(msg) > 0 {
+				if delimiter == '\n' {
+					fmt.Print("Msg Received:", string(msg))
+				} else {
+					fmt.Println("Msg Received:", string(msg))
+				}
 
-		if len(msg) > 0 {
-			if delimiter == '\n' {
-				fmt.Print("Msg Received:", string(msg))
+				// newMsg := strings.ToUpper(strings.TrimSuffix(msg, string(delimiter)))
 			} else {
-				fmt.Println("Msg Received:", string(msg))
+				fmt.Println("Closed from client.")
+				con.Close()
+				con, _ = ln.Accept()
 			}
-
-			newMsg := strings.ToUpper(strings.TrimSuffix(msg, string(delimiter)))
-			con.Write([]byte(newMsg + string(delimiter)))
-		} else {
-			fmt.Println("Closed from client.")
-			con.Close()
-			con, _ = ln.Accept()
 		}
 	}
-	fin <- 1
+	// fin <- 1
 }
 
 func main() {
 	fin := make(chan int)
 	go pcListener(fin)
-	<-fin
+	// go func() {
+	i := 0
+	for {
+		log.Println("hahaha" + fmt.Sprint(i))
+		Msg <- "hahaha" + fmt.Sprint(i)
+		time.Sleep(time.Second * 1)
+		if i%2 == 0 {
+			Health <- "health"
+		}
+		i++
+	}
+	// }()
+	// <-fin
 }
